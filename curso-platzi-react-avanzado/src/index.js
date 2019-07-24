9f64c236d6718862eeb555b451adab6081a7e2d0
RamonBoza <ramon.boza@HUB000127.local>
2019-07-24 16:37:46 +0200

    Adding latest dependencies with babel to add jsx

--- curso-platzi-react-avanzado/src/index.js
+++ curso-platzi-react-avanzado/src/index.js
@@ -1,4 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render('Hola Platzi', document.getElementById('app'));
+ReactDOM.render(<h1>'Seguimos con el curso avanzado'</h1>, document.getElementById('app'));

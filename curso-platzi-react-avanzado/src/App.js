2864e92f6919d2554a342b95ae914817f8aebec8
RamonBoza <ramon.boza@HUB000127.local>
2019-07-26 18:57:21 +0200

    Class 11 - adding logo as svg

--- curso-platzi-react-avanzado/src/App.js
+++ curso-platzi-react-avanzado/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import {ListOfCategories} from "./components/ListOfCategories";
 import { ListOfPhotoCards } from "./components/ListOfPhotoCards";
+import { Logo } from "./components/Logo";
 import { GlobalStyle } from './GlobalStyles'
 
 export const App = () => (
   <div>
+    <Logo/>
     <GlobalStyle/>
     <ListOfCategories />
     <ListOfPhotoCards />

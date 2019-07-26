c18864af08ceb8ae9a2c04c94367f246a6dc80a6
RamonBoza <ramon.boza@HUB000127.local>
2019-07-26 18:38:23 +0200

    Adding PhotoCard component

--- curso-platzi-react-avanzado/src/App.js
+++ curso-platzi-react-avanzado/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {ListOfCategories} from "./components/ListOfCategories";
+import { PhotoCard } from "./components/PhotoCard";
 import { GlobalStyle } from './GlobalStyles'
 
 export const App = () => (
   <div>
     <GlobalStyle/>
     <ListOfCategories />
+    <PhotoCard />
   </div>
 
 );

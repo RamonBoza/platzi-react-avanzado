a6708278898055dff6b6a60c1d72b2b0d1ec1855
RamonBoza <ramon.boza@HUB000127.local>
2019-07-29 17:49:39 +0200

    Class 13 - Adding categories from our server in now

--- curso-platzi-react-avanzado/src/components/ListOfCategories/index.js
+++ curso-platzi-react-avanzado/src/components/ListOfCategories/index.js
@@ -1,12 +1,18 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import { Category } from "../Category";
 import { List, Item } from './styles'
 
-import { categories as mockCategories} from '../../../api/db.json'
-
-
 export const ListOfCategories = () => {
-  const [categories, setCategories] = useState(mockCategories)
+  const [categories, setCategories] = useState([])
+
+  useEffect(function() {
+    window.fetch('https://petgram-server-boza.ramonboza.now.sh/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+
+  }, [])
   return (
     <List>
       {

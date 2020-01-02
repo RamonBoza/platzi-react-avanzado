b0e6ea58855dd69fd37fe2b8a22f79cf043cfc04
Ramon Boza <ramonboza@Ramon-Boza.local>
2020-01-02 17:33:24 +0100

    Class 19 - Adding hooks to control better the code and reuse

--- curso-platzi-react-avanzado/src/components/PhotoCard/index.js
+++ curso-platzi-react-avanzado/src/components/PhotoCard/index.js
@@ -1,22 +1,19 @@
 import React, {Fragment, useEffect, useRef, useState} from 'react'
 import { Article, ImgWrapper, Img, Button } from "./styles";
 import { MdFavoriteBorder, MdFavorite} from "react-icons/md";
+import { useLocalStorage } from "../../hooks/useLocalStorage";
 
 const DEFAULT_IMAGE = "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60";
+
+
 
 export const PhotoCard = ( { id, likes = 0, src = DEFAULT_IMAGE }) => {
 
   const element = useRef(null)
   const [show, setShow] = useState(false)
   const key = `like-${id}`;
-  const [liked, setLiked] = useState(() => {
-    try {
-      return window.localStorage.getItem(key)
-    } catch (e) {
-      return false;
-    }
+  const [liked, setLiked] = useLocalStorage(key, false)
 
-  })
 
   console.log(liked)
 
@@ -33,14 +30,7 @@ export const PhotoCard = ( { id, likes = 0, src = DEFAULT_IMAGE }) => {
 
   const Icon = liked ? MdFavorite : MdFavoriteBorder
 
-  const setLocalStorage = value => {
-    try {
-      window.localStorage.setItem(key, value)
-      setLiked(value)
-    } catch (e) {
-      console.log(e)
-    }
-  }
+
 
   return (
     <Article ref={element}>
@@ -52,7 +42,7 @@ export const PhotoCard = ( { id, likes = 0, src = DEFAULT_IMAGE }) => {
             </ImgWrapper>
           </a>
 
-          <Button onClick={() => setLocalStorage(!liked)}>
+          <Button onClick={() => setLiked(!liked)}>
             <Icon size='32px' /> {likes} likes!
           </Button>
         </Fragment>
